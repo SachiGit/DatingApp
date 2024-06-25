09bd0248f5a1b4c742e804d94fc339c92fd48737
Sachith Thilina <sachithilina@hotmail.com>
2024-06-25 22:11:08 +0530

    first commit

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+        public DbSet<AppUser> Users { get; set; } 
+}
+
+//public class DataContext : DbContext
+//{
+    //public DataContext(DbContextOptions<DataContext> options) : base(options)
+    //{
+        //Database.EnsureCreated();
+    //}
+
+    //public DbSet<AppUser> Users { get; set; }
+//}
+
