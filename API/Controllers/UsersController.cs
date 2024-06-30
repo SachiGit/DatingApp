74b2f0c4d8ce93fd1d2a7c799902068a40f87541
Sachith Thilina <sachithilina@hotmail.com>
2024-06-30 15:46:52 +0530

    [ApiController] [Route("api/[controller]")] added

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API;
+
+[ApiController]
+[Route("api/[controller]")]  // /api/users
+public class UsersController(DataContext context) : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        var users = context.Users.ToList();
+
+        return users;
+    }
+
+    [HttpGet("{id:int}")]    //  /api/users/1/2/3/4/5
+    public ActionResult<AppUser> GetUsers(int id)
+    {
+        var user = context.Users.Find(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+        return user;
+    }
+}
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efd5413187ead1029f946dee8d4d77fa016b56b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b08f2cb09614f5b63bd65923ea6bb965452eb1c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
